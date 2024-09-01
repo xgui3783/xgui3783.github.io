7e51143bd6c42a1651bbb03c40050abbdac1ffbc
Xiao Gui <xgui3783@gmail.com>
2024-09-01 11:41:10 +0200

    fix: update 11ty version

---
+++ _data/metadata.js
@@ -0,0 +1,17 @@
+module.exports = {
+	title: "pandamakes",
+	url: "https://pandamakes.me/",
+	description: "web developer, photographer, panda",
+	feed: {
+	  subtitle: "web developer, photographer, panda",
+	  filename: "feed.xml",
+	  path: "/feed/feed.xml",
+	  url: "https://pandamakes.me/feed/feed.xml",
+	  id: "https://pandamakes.me/"
+	},
+	author: {
+	  name: "panda",
+	  email: "panda@pandamakes.me"
+	}
+  }
+  
